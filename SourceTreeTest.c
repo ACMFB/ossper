d02990c34529a8ac221ab692c4438b2632aed9fe
EUIHEOK <uihyeogjeong@gmail.com>
2025-10-29 17:21:06 +0900

    Test 브랜치에 코드 추가

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main() {
+
+    int i = 0;
+
     printf("First oss code\n");
     printf("Second oss code\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("Test 브랜치\n");
+    }
+    
     return 0;
 }
